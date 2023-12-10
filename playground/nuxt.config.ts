78475325ee35c4395e68c5fdb0315e94f03e51b2
alvarosabu <alvaro.saburido@gmail.com>
2023-12-10 13:01:41 +0100

    feat: add vite-plugin-glsl via options

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,13 +1,12 @@
-import glsl from 'vite-plugin-glsl'
-
 export default defineNuxtConfig({
   modules: ['../src/module', '@nuxt/devtools'],
   tres: {
     // for testing purposes, and so we test both deduplication + auto-detection capabilities
     modules: ['@tresjs/cientos'],
     devtools: true,
+    glsl: true,
   },
-  vite: {
+/*   vite: {
     plugins: [glsl()],
-  },
+  }, */
 })
