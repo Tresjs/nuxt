51bf9ac8cffc7647e106f8b5870e88a172b5e8fa
alvarosabu <alvaro.saburido@gmail.com>
2024-12-19 11:37:27 +0100

    chore: trying to update deps

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,17 +2,20 @@ import { resolve } from 'pathe'
 
 export default defineNuxtConfig({
   ssr: false,
+
   modules: [
     '@nuxt/devtools-ui-kit',
     '@unocss/nuxt',
     '@nuxt/ui',
     '@nuxt/icon',
   ],
+
   nitro: {
     output: {
       publicDir: resolve(__dirname, '../dist/client'),
     },
   },
+
   icon: {
     size: '24px', // default <Icon> size applied
     class: 'icon', // default <Icon> class applied
@@ -20,7 +23,10 @@ export default defineNuxtConfig({
       mesh: 'carbon:cube',
     },
   },
+
   app: {
     baseURL: '/__tres_nuxt_devtools',
   },
+
+  compatibilityDate: '2024-12-19',
 })
