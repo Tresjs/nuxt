1f318777ac8f25fd623280077aac56f78e658689
alvarosabu <alvaro.saburido@gmail.com>
2025-07-25 18:25:11 +0200

    feat(client): enhance PerformanceMonitor component with improved UX and detailed statistics
    - Added computed properties for performance and memory status to provide real-time feedback on system performance.
    - Refactored the template structure to utilize `UCard` and `UBadge` components for better visual representation.
    - Introduced detailed statistics for memory and render performance, including current and peak memory usage.
    - Updated `index.vue` to integrate the `PerformanceMonitor`, enhancing the overall user experience in monitoring performance metrics.

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -46,92 +46,11 @@ const tabs = [
       >
         <template #scene-graph>
           <SceneGraph />
-          <!-- <SceneGraphItem :item="scene.graph" /> -->
         </template>
         <template #performance>
-          <!-- <PerformanceMonitor /> -->
+          <PerformanceMonitor />
         </template>
       </UTabs>
     </main>
   </div>
-  <!-- <div class="relative n-bg-base flex flex-col h-screen">
-    <header
-      class="p4 flex items-center justify-between hover:bg-active"
-      border="b base"
-    >
-      <div class="flex items-center gap-4">
-        <img
-
-          src="/logo.svg"
-          alt="tres logo"
-        >
-        <h2 class="opacity-60 font-bold">
-          TresJS DevTools
-        </h2>
-      </div>
-      <div class="flex items-center gap-2">
-        <UButton
-          variant="ghost"
-          color="white"
-          size="sm"
-          icon="i-carbon-document"
-          target="_blank"
-          to="https://docs.tresjs.org/"
-        />
-        <UButton
-          variant="ghost"
-          color="white"
-          size="sm"
-          icon="i-iconoir-github"
-          target="_blank"
-          to="https://github.com/Tresjs/nuxt"
-        />
-      </div>
-    </header>
-
-    <div
-      v-if="client && scene.objects > 0"
-      class="flex flex-col gap-2"
-    >
-      <NSectionBlock
-        icon="i-iconoir-movie"
-        text="Scene Graph"
-        :description="`Total Objects ${scene.objects}`"
-      >
-        <SceneGraphItem :item="scene.graph" />
-      </NSectionBlock>
-      <NSectionBlock
-        icon="i-iconoir-dashboard-speed"
-        text="Performance"
-      >
-        <template #actions>
-          <NBadge
-            n="green"
-            class="text-[#15803D] dark:text-[#34E676]"
-          >
-            FPS: {{ fps.value }}
-          </NBadge>
-          <NBadge
-            n="yellow"
-            class="text-[#827717] dark:text-[#EAB306]"
-          >
-            Memory: {{ Math.round(memory?.currentMem) }}MB
-          </NBadge>
-        </template>
-        <template #default>
-          <PerformanceMonitor />
-        </template>
-      </NSectionBlock>
-    </div>
-    <div v-else-if="scene.objects === 0">
-      <div class="p4 h-full">
-        <NLoading />
-      </div>
-    </div>
-    <div v-else>
-      <NTip n="yellow">
-        Failed to connect to the client. Did you open this page inside Nuxt DevTools?
-      </NTip>
-    </div>
-  </div> -->
 </template>
