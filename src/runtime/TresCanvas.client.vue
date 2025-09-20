16f12e79334c6dcbd3d78412838b41a00a47185d
alvarosabu <alvaro.saburido@gmail.com>
2025-09-20 11:07:29 +0200

    fix: integrate useAttrs composable in TresCanvas component
    - Added `useAttrs` import to enhance attribute handling in the `TresCanvas.client.vue` component, allowing for better integration of attributes passed to the component.

--- src/runtime/TresCanvas.client.vue
+++ src/runtime/TresCanvas.client.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
+import { useAttrs } from '#imports'
 import { TresCanvas as CoreTresCanvas } from '@tresjs/core'
 
 const attrs = useAttrs()
-
-console.log({ attrs })
 </script>
 
 <template>
